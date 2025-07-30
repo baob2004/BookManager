153f56b2b0b9cd34d96b83ecfbf5fc790125cbde
DinhQuocBao <baob2004@gmail.com>
2025-07-30 17:00:16 +0700

    feat: AuthController Register and Login User

---
+++ BookstoreAPI/Dtos/Account/RegisterDto.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace BookstoreAPI.Dtos.Account
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string Address { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}
