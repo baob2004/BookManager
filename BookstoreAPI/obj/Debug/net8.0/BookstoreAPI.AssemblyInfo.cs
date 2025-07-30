05d5db2b5bc5c8bf48a2e763e521684e5fb8214c
DinhQuocBao <baob2004@gmail.com>
2025-07-30 15:27:10 +0700

    Create Application User with basic settings

--- BookstoreAPI/obj/Debug/net8.0/BookstoreAPI.AssemblyInfo.cs
+++ BookstoreAPI/obj/Debug/net8.0/BookstoreAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookstoreAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+111a415f0da6317d440a270d76f5b4b964f20fd7")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookstoreAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookstoreAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
